23adeabe635d3efc4b8f24fc65141c6e55c31b31
moazzamkhan112 <164583812+moazzamkhan112@users.noreply.github.com>
2024-04-05 03:41:52 +0500

    atm machine first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condtion = true;
+while (condtion)
+    ;
+let todosQuestion = await inquirer.prompt([
+    {
+        name: "firstQuestion",
+        message: "what would you like to add in your todo",
+        type: "input",
+    },
+    {
+        name: "secondQuestion",
+        type: "confirm",
+        message: "what woul you like to add in your todo",
+        default: "true"
+    }
+]);
+todos.push(todosQuestion.firstQuestion);
+console.log(todos);
+condtion = (todosQuestion.secondQuestion);
